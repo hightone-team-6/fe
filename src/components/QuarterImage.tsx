186f5ec121685cce9bacab4f971f2ad8c1d8cc8f
frorong <moondgod@gmail.com>
2025-02-15 21:51:52 +0900

    quater image

---
+++ src/components/QuarterImage.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 4px;
+
+  width: 170px;
+  height: 170px;
+
+  position: relative;
+  overflow: hidden;
+  border-radius: 12px;
+`;
+
+interface QuarterImageProps {
+  images: [string, string, string, string];
+}
+
+export const QuarterImage = ({ images }: QuarterImageProps) => {
+  return (
+    <Grid>
+      {images.map((image) => (
+        <img width={82} height={82} src={image} alt="quarter" />
+      ))}
+    </Grid>
+  );
+};
